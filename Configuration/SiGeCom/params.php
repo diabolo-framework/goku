f462cd4b5b3a8be6b9db5c8e653c76ccb5862645
Michael Luthor <michaelluthor@163.com>
2018-12-02 19:38:04 +0800

    delete files

--- Configuration/SiGeCom/params.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-# whether allow register new user, default to true
-'enableRegistion' => true,
-# email verfication lift time in seconds
-'emailVerficationLifeTime' => 3600, 
-# if https enabled
-'enableHttps' => false,
-);
